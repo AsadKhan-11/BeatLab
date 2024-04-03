fe7f1e2bedb2326c518c90992394c806692c4917
Asad Khan <mrasad10khan@gmail.com>
2024-04-03 13:45:47 +0500

    Adding Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <>
+      <section className="navbar-sec">
+        <h2 className="nav-icon">BeatLab</h2>
+      </section>
+    </>
+  );
+}
+
+export default Navbar;
