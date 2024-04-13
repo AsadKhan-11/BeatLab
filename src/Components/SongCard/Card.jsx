6db89dc16980e8b559fce0d4a3be2c62c78c3e12
Asad Khan <mrasad10khan@gmail.com>
2024-04-13 19:01:08 +0500

    Adding control btns for song cards

--- src/Components/SongCard/Card.jsx
+++ src/Components/SongCard/Card.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import "./Card.css";
 import Song from "./Song/Song";
+import image1 from "./Song/Pictures/ys.jpg";
+import image2 from "./Song/Pictures/red.jpg";
+import image3 from "./Song/Pictures/marsh.jpg";
 
 function Card() {
   var ys = new Audio("./Songs/ys.mp3");
@@ -8,18 +11,18 @@ function Card() {
   var friends = new Audio("./Songs/FRIENDS.mp3");
 
   const songs = [
-    { song: ys, name: "Therapy time", img: "./Song/Pictures/ys.jpg" },
-    { song: red, name: "Red Challenger", img: "./Song/Pictures/red.jpg" },
-    { song: friends, name: "FRIENDS", img: "./Song/Pictures/marsh.jpg" },
+    { song: ys, name: "Therapy time", img: image1 },
+    { song: red, name: "Red Challenger", img: image2 },
+    { song: friends, name: "FRIENDS", img: image3 },
   ];
-  console.log(songs);
   return (
     <>
       <section className="card-sec">
         {songs.map((song, index) => {
+          console.log(songs);
           return (
             <div key={index}>
-              <Song name={song.name} img={song.img} />;
+              <Song name={song.name} img={song.img} />
             </div>
           );
         })}
--- src/Components/SongCard/Song/Song.jsx
+++ src/Components/SongCard/Song/Song.jsx
@@ -10,6 +10,52 @@ function Song(props) {
           className="song-img"
         />
         <h2 className="song-name">{props.name}</h2>
+
+        <div className="control-btns">
+          <div className="prev-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z"
+              />
+            </svg>
+          </div>
+          <div className="play-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+              />
+            </svg>
+          </div>
+
+          <div className="next-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
     </>
   );
