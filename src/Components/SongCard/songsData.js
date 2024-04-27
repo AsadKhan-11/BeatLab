ef0eb38add1d242d235ad43fe5a28facf181f628
Asad Khan <mrasad10khan@gmail.com>
2024-04-27 22:57:20 +0500

    Adding Songs

--- src/Components/SongCard/songsData.js
+++ src/Components/SongCard/songsData.js
@@ -1,14 +1,20 @@
 import image1 from "./Song/Pictures/ys.jpg";
 import image2 from "./Song/Pictures/red.jpg";
 import image3 from "./Song/Pictures/marsh.jpg";
+import image4 from "./Song/Pictures/ApD.webp";
+import image5 from "./Song/Pictures/rock.jpg";
 import ys from "./Song/Songs/ys.mp3";
 import red from "./Song/Songs/Babbu.mp3";
 import friends from "./Song/Songs/FRIENDS.mp3";
+import miAmor from "./Song/Songs/Mi Amor.mp3";
+import rockabye from "./Song/Songs/Rockabye.mp3";
 
 const songs = [
   { song: ys, name: "Therapy time", img: image1 },
   { song: red, name: "Red Challenger", img: image2 },
   { song: friends, name: "FRIENDS", img: image3 },
+  { song: miAmor, name: "Mi Amor", img: image4 },
+  { song: rockabye, name: "Rockabye", img: image5 },
 ];
 
 export default songs;
