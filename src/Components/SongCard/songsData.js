2814f452824c8a7dd4475c8a8c1d80b5c1725bc4
Asad Khan <mrasad10khan@gmail.com>
2024-04-29 23:01:31 +0500

    Adding songs and pictures

--- src/Components/SongCard/songsData.js
+++ src/Components/SongCard/songsData.js
@@ -3,18 +3,40 @@ import image2 from "./Song/Pictures/red.jpg";
 import image3 from "./Song/Pictures/marsh.jpg";
 import image4 from "./Song/Pictures/ApD.webp";
 import image5 from "./Song/Pictures/rock.jpg";
+import image6 from "./Song/Pictures/despacito.jpg";
+import image7 from "./Song/Pictures/yimmy.jpg";
+import image8 from "./Song/Pictures/whynot.jpg";
+import image9 from "./Song/Pictures/summerhigh.jpg";
+import image10 from "./Song/Pictures/withyou.jpg";
+import image11 from "./Song/Pictures/darklove.jpg";
+import image12 from "./Song/Pictures/kingshit.jpg";
+
 import ys from "./Song/Songs/ys.mp3";
 import red from "./Song/Songs/Babbu.mp3";
 import friends from "./Song/Songs/FRIENDS.mp3";
 import miAmor from "./Song/Songs/Mi Amor.mp3";
 import rockabye from "./Song/Songs/Rockabye.mp3";
+import Despacito from "./Song/Songs/Despacito.mp3";
+import Yimmy from "./Song/Songs/Yimmy.mp3";
+import Whynot from "./Song/Songs/Why not.mp3";
+import Kingshit from "./Song/Songs/King shit.mp3";
+import DarkLove from "./Song/Songs/Dark Love.mp3";
+import WithYou from "./Song/Songs/With You.mp3";
+import SummerHigh from "./Song/Songs/Summer High.mp3";
 
 const songs = [
-  { song: ys, name: "Therapy time", img: image1 },
-  { song: red, name: "Red Challenger", img: image2 },
+  { song: Kingshit, name: "King Shit", img: image12 },
+  { song: SummerHigh, name: "Summer High", img: image9 },
+  { song: Yimmy, name: "Yimmy Yimmy", img: image7 },
+  { song: DarkLove, name: "Dark Love", img: image11 },
   { song: friends, name: "FRIENDS", img: image3 },
+  { song: Despacito, name: "Despacito", img: image6 },
+  { song: ys, name: "Therapy time", img: image1 },
   { song: miAmor, name: "Mi Amor", img: image4 },
   { song: rockabye, name: "Rockabye", img: image5 },
+  { song: Whynot, name: "Why not meri jaan", img: image8 },
+  { song: WithYou, name: "With You", img: image10 },
+  { song: red, name: "Red Challenger", img: image2 },
 ];
 
 export default songs;
